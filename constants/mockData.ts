f7afab17bbab4dbb4bda82ec0d2841754443f2b5
Raiyan Islam <rmhislam@gmail.com>
2026-03-14 20:47:11 -0400

    Add constants/mockData.ts

---
+++ constants/mockData.ts
@@ -0,0 +1,3 @@
+export type { PrayerName } from "@/types/models";
+export { PRAYER_NAMES } from "@/types/models";
+export type { Post, PostWithUser, User, Reel, Creator, ExploreCategory } from "@/types/models";
