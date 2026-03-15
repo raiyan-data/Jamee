0b72a9469c46e79f58467e297552a9f80a6d935e
Raiyan Islam <rmhislam@gmail.com>
2026-03-14 20:46:56 -0400

    Add components/index.ts

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+export { Avatar } from "./Avatar";
+export { LikeButton } from "./LikeButton";
+export { PostCard } from "./PostCard";
+export { CommentItem, type CommentData } from "./CommentItem";
+export { VideoPlayer } from "./VideoPlayer";
+export type { ReelData } from "./VideoPlayer";
+export { ProfileHeader, type ProfileStats, type Badge } from "./ProfileHeader";
+export { ProgressRing, MultiProgressRing } from "./ProgressRing";
+export { CircularProgress } from "./CircularProgress";
+export { PrayerRow } from "./PrayerRow";
+export {
+  BottomNavigationBar,
+  DEFAULT_TABS,
+  type NavTab,
+} from "./BottomNavigationBar";
+export { CommentsSheet } from "./CommentsSheet";
+export { ErrorBoundary } from "./ErrorBoundary";
